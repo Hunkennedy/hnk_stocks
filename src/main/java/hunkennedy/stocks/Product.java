7a8bdf9f48f8e402aa7681142d363967a617eaf2
Julio Aguilar <cuentas.96@live.com>
2023-12-04 19:03:36 -0300

    Product Entity

--- src/main/java/hunkennedy/stocks/Product.java
+++ src/main/java/hunkennedy/stocks/Product.java
@@ -1,4 +1,13 @@
 package hunkennedy.stocks;
 
+import java.util.Date;
+
 public class Product {
+    private int id;
+    private String name;
+    private String supplier;
+    private String category;
+    private Date created;
+    private Date updated;
+    private String measurement;
 }
